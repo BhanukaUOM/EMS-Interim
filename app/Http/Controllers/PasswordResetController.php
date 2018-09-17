5ec0bb233b75b3cd6cc16720b3356126f99e9fbc
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 18:56:29 +0530

    reset password email check

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class PasswordResetController extends Controller
 {
     public function sendEmail(Request $request){
-        return "aaaaa";
+        if(!validateEmail($request->email)){
+            return response()->json(
+                ['error' => 'Email not Registered!!'], Response::HTTPNOTFOUND
+            );
+        }
+    }
+
+    public function validateEmail($email){
+        return !!User::where('email', $email)->first();
     }
 }
