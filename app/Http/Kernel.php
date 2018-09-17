16d675190888b033283c191af996a16a61839c54
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-18 00:17:11 +0530

    a

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         //\App\Http\Middleware\CORS::class,
-        \Barryvdh\Cors\HandleCors::class,
+        //\Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
+            //\Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
