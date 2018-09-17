8f6c4f7eaac98c816dabf011643cd131abb2b21f
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 21:43:16 +0530

    a

--- public/main.js
+++ public/main.js
@@ -760,8 +760,6 @@ var ResponseResetComponent = /** @class */ (function () {
     };
     ResponseResetComponent.prototype.resetHandler = function (data) {
         this.notify.info(data.data, { timeout: 2000 });
-        this.token.set(data);
-        this.router.navigateByUrl('/dashboard');
     };
     ResponseResetComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
