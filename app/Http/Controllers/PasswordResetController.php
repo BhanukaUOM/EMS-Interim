e7b2ea182d0b52647c8f47edb7d0d10afdccce7c
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 19:14:46 +0530

    password reset mail set

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -13,6 +13,11 @@ class PasswordResetController extends Controller
             return response()->json(
                 ['error' => 'Email not Registered!!'], Response::HTTP_NOT_FOUND
             );
+        } else {
+            Mail::to($request->email)->send(new ResetPasswordMail);
+            return response()->json(
+                ['data' => 'Reset email sent sucessfully!! \nPlease check mail inbox.'], Response::HTTP_OK
+            );
         }
     }
 }
