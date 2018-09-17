c7215a123d772adf315b97f9018786eaed648d55
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 21:37:39 +0530

    a

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -36,15 +36,23 @@ class PasswordResetController extends Controller
         }
     }
 
-    public function reset(ChangePasswordRequest $request){
-        //return 'a';
-        if(DB::table('password_resets')->where(['email' => $request->email,'token' =>$request->resetToken])->count()> 0){
-            $user = User::whereEmail($request->email)->first();
-            $user->update(['password'=>$request->password]);
-            $this->DB::table('password_resets')->where(['email' => $request->email,'token' =>$request->delete()]);
-            return response()->json(['data'=>'Password Successfully Changed'],Response::HTTP_CREATED);
-        } else {
-            return response()->json(['error' => 'Token or Email is incorrect'],Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+    public function reset(ChangePasswordRequest $request)
+    {
+        return $this->getPasswordResetTableRow($request)->count()> 0 ? $this->changePassword($request) : $this->tokenNotFoundResponse();
+    }
+    private function getPasswordResetTableRow($request)
+    {
+        return DB::table('password_resets')->where(['email' => $request->email,'token' =>$request->resetToken]);
+    }
+    private function tokenNotFoundResponse()
+    {
+        return response()->json(['error' => 'Token or Email is incorrect'],Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+    private function changePassword($request)
+    {
+        $user = User::whereEmail($request->email)->first();
+        $user->update(['password'=>$request->password]);
+        $this->getPasswordResetTableRow($request)->delete();
+        return response()->json(['data'=>'Password Successfully Changed'],Response::HTTP_CREATED);
     }
 }
