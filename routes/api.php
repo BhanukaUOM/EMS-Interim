80e8cad0a3e17bb249f1d18bc83d4aaa99d8efee
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 14:12:34 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,5 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-    //Route::get('/{any}', function () {
-      //  return 'Unortharized!';
-    //});
+
 });
