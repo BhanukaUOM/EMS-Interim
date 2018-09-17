d6e0ead2ee33d78876e2e4f606dfd589e301f874
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 19:17:29 +0530

    a

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Controllers\Mail;
 
 class PasswordResetController extends Controller
 {
