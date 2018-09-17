71e7654c1cfa5f1bb61bd55ac8534c4b8fc0bff5
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-17 23:33:46 +0530

    a

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -88,6 +88,6 @@ class AuthController extends Controller
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
             'user' => auth()->user()
-        ])->header('Access-Control-Allow-Origin', '*');
+        ]);
     }
 }
