5dc464f76e2a805d6535251dac1890f6bf8f72d5
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-19 00:19:46 +0530

    a

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -12,6 +12,6 @@ class NoticeController extends Controller
 {
     public function getNotice(Request $request){
         $token = $request->access_token;
-        return base64_decode(explode('.', $token)[1]);
+        return (Json.parse(base64_decode(explode('.', $token)[1]))->iss=='https:\/\/ems.aladinlabs.com\/api\/login')?true:false;
     }
 }
