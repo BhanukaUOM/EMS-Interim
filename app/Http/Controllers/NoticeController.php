7433eb41ebab909dc45fb49f3f0c0ff48fa94dd1
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-19 00:57:27 +0530

    a

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -22,7 +22,7 @@ class NoticeController extends Controller
             $role = $user->role;
             $id = $user->id;
             //return DB::select('select * from notice, readstatus  where readstatus.userId = ?', $id);
-            return DB::select('select * from notice');
+            return DB::select('select * from notice where role= ?', $role);
         } else {
             return response()->json(['error' => 'Token incorrect'],Response::HTTP_UNPROCESSABLE_ENTITY);
         }
