5ef0d9698499f65a991507b0631fcec18c3f22c7
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-09-19 00:07:04 +0530

    a

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -12,25 +12,7 @@ class NoticeController extends Controller
 {
     public function getNotice(Request $request){
         $token = $request->access_token;
-        //return $token;
         //$user = User::where('auth'. $token);
-        //return JWTAuth::getPayload($token)->toArray();
-        try {
-            // attempt to verify the credentials and create a token for the user
-            $token = JWTAuth::getToken();
-            $apy = JWTAuth::getPayload($request->access_token)->toArray();
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-    
-            return response()->json(['token_expired'], 500);
-    
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-    
-            return response()->json(['token_invalid'], 500);
-    
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-    
-            return response()->json(['token_absent' => $e->getMessage()], 500);
-    
-        }
+        return JWTAuth::decode($token);
     }
 }
